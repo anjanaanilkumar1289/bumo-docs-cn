b540f77d4c4a18960c7725a7828feb2aa42e7fc4
wilsonyou <40747875+wilsonyou@users.noreply.github.com>
2018-10-25 09:54:08 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'bumo'
-copyright = '2018, wilson'
+copyright = '2018, Bumo'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
