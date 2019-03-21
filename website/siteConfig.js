7b262a2e43695dbed0c3609d5d2d502dda075012
黄文海 <huangwenhai@bubi.cn>
2019-03-21 11:56:28 +0800

    fix switch english-site

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -45,7 +45,7 @@ const siteConfig = {
   headerLinks: [
     {doc: 'introduction_to_bumo', label: '文档'},
     { href: "https://github.com/bumoproject/bumo", label: "GitHub", target: "_blank" },
-    { href: "https://docs.bumo.io", label: "English", target: "_self" },
+    { href: "javascript:(0);", label: "English", target: "_self" },
     { languages: true },
     { search: true }
   ],
